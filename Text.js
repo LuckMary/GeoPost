9a553c035ca59a9bd098aacc95f58d88e22d5fb9
Mary Lukicheva <luckmary8@gmail.com>
2021-10-14 18:10:18 +0300

    add copying

--- Text.js
+++ Text.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import Clipboard from '@react-native-clipboard/clipboard';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MyText = ({text}) => {
   const [coords, setCoords] = useState({});
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     Geolocation.getCurrentPosition(info => {
@@ -15,15 +18,67 @@ const MyText = ({text}) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+    }
+  }, [copied]);
+
   return (
-    <View>
-      {Object.keys(coords).map(key => (
-        <Text key={key}>
-          {key}: {coords[key]}
-        </Text>
-      ))}
+    <View style={styles.coordinatesBar}>
+      <Text>{`latitude: ${coords?.latitude}`}</Text>
+      <Text>{`longitude: ${coords?.longitude}`}</Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          {
+            backgroundColor: copied ? '#F0FFF0' : 'lightgrey',
+            borderColor: copied ? 'green' : 'grey',
+          },
+        ]}
+        onPress={
+          copied
+            ? null
+            : () => {
+                Clipboard.setString(
+                  `${coords?.latitude}, ${coords?.longitude}`,
+                );
+                setCopied(true);
+              }
+        }
+        activeOpacity={copied ? 1 : 0.5}>
+        <Text style={{marginEnd: 10}}>{`${copied ? 'Copied' : 'Copy'} `}</Text>
+        <Icon
+          name={`${copied ? 'check' : 'content-copy'}`}
+          color={`${copied ? 'green' : 'grey'}`}
+          size={20}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    flexDirection: 'row',
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  coordinatesBar: {
+    borderWidth: 2,
+    borderColor: 'red',
+    borderRadius: 5,
+    borderStyle: 'solid',
+    padding: 5,
+    paddingBottom: 0,
+  },
+});
 
 export default MyText;
